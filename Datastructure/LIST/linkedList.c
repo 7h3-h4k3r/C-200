c2720a833587c480fc4dae3b3c73825806f57fb7
sridharanitharan <username420sri@gmail.com>
2025-11-22 20:26:52 +0530

    pop(),traverse output()

--- Datastructure/LIST/linkedList.c
+++ Datastructure/LIST/linkedList.c
@@ -7,17 +7,28 @@ typedef struct Node{
 }Node;
 
 void push(Node **head,int data);
-
-
+int peek(Node **head);
+int del(Node **head, int remove);
+void output(Node **head);
+int pop(Node **head);
 int main(){
     Node* head = NULL;
     push(&head,10);
     push(&head,20);
+    push(&head,30);
+    push(&head,40);
+    push(&head,50);
+    peek(&head);
+    del(&head,30);
+    del(&head,40);
+    printf(" pop () %d \n",pop(&head));
+    printf(" Treaverse output() \n");
+    output(&head);
     
 }
 
 
-void push(Node** head,int data){
+void push(Node **head,int data){
     Node * new_node = (Node*)malloc(sizeof(Node));
     if (!new_node){
         perror("Memory allocation Failed");
@@ -30,4 +41,60 @@ void push(Node** head,int data){
         new_node->next = *(head);
         *(head) = new_node;
     }
+}
+
+int peek(Node **head){
+    if (*(head) == NULL){
+        fprintf(stderr,"List is Empty");
+        return -1;
+    }
+
+    return (*head)->data;
+}
+int pop(Node **head){
+    if ((*head) == NULL){
+        fprintf(stderr,"List is Empty");
+        return -1;
+    }
+    int value = (*head)->data;
+    *head = (*head)->next;
+    return value;
+}
+int del(Node **head,int remove){
+    if (*(head) == NULL){
+        fprintf(stderr,"List is Empty ");
+        return -1;
+    }
+    
+   Node *temp = *head;
+   Node *prev = NULL;
+
+   while (temp)
+   {   
+        if (temp->data == remove){
+            prev->next = temp->next;
+            break; 
+        }
+        prev = temp;
+        temp = temp->next;
+   }
+
+
+
+}
+
+void output(Node **head){
+    Node *last =  *head;
+    while (last)
+    {
+        if(last->next){
+            printf(" %d -> ",last->data);
+        }
+        else{
+            printf("%d ",last->data);
+        }
+        
+        last = last->next;
+    }
+    printf("\n");
 }
